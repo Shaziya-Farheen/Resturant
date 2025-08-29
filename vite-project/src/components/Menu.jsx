436489307fcda815627c14ddf9bc474976127044
Shaziya Farheen <shazii2605@gmail.com>
2025-08-29 07:06:40 -0700

    Uploading frontend files

---
+++ vite-project/src/components/Menu.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import {data} from '../restApi.json'
+const Menu = () => {
+  return (
+    <>
+      <section className='menu' id='menu'>
+        <div className="container">
+            <div className="heading_section">
+                <h1 className="heading">POPULAR DISHES</h1>
+                <p>A genuine fine-dining experience awaits. 
+                  Come for a drink, stay for a meal. Great food and great company.
+                   Nothing brings people together like good food !!</p>
+            </div>
+            <div className="dishes_container">
+                {
+                    data[0].dishes.map(element => (
+                        <div className="card" key={element.id}>
+                                <img src={element.image} alt={element.title} />
+                                <h3>{element.title}</h3>
+                                <button>{element.category}</button>
+                        </div>
+                    ))
+                }   
+            </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Menu
